dd2ac430268c359e5411dd7010a67028d41e1218
Edwin Christie <echristie@bluecatnetworks.com>
2023-04-25 17:20:02 -0400

    Upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,37 @@
 const path = require('path');
-
 module.exports = {
-    stories: ['./welcome.stories.mdx', '../stories/**/*.stories.*'],
-    addons: [
-        { name: '@storybook/addon-essentials', options: { backgrounds: false } },
-        '@storybook/addon-a11y',
-    ],
+  stories: ['./welcome.stories.mdx', '../stories/**/*.stories.*'],
+  addons: [{
+    name: '@storybook/addon-essentials',
+    options: {
+      backgrounds: false
+    }
+  }, '@storybook/addon-a11y'],
+  core: {
+    disableTelemetry: true
+  },
+  webpackFinal: async (config, {
+    configType
+  }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
 
-    core: {
-        builder: 'webpack5',
-        disableTelemetry: true,
-    },
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.less$/,
+      use: ['style-loader', 'css-loader', 'less-loader'],
+      include: path.resolve(__dirname, '../')
+    });
 
-    webpackFinal: async (config, { configType }) => {
-        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-        // You can change the configuration based on that.
-        // 'PRODUCTION' is used when building the static version of storybook.
-
-        // Make whatever fine-grained changes you need
-        config.module.rules.push({
-            test: /\.less$/,
-            use: ['style-loader', 'css-loader', 'less-loader'],
-            include: path.resolve(__dirname, '../'),
-        });
-
-        // Return the altered config
-        return config;
-    },
+    // Return the altered config
+    return config;
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
